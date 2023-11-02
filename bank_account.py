68fe8cc65945b2414c0d8b79a8c8eacc0d7ede9f
Pattapon Gowanit <iampattapon@gmail.com>
2023-11-02 10:04:52 +0700

    Add the delete method to AccountDB

--- bank_account.py
+++ bank_account.py
@@ -21,6 +21,14 @@ class AccountDB:
                 return account
         return None
 
+    def delete(self, account_num):
+        index = self.__search_private(account_num)
+        if index != -1:
+            del self.account_database[index]
+            print(f"Account {account_num} has been deleted.")
+        else:
+            print(f"Account {account_num} not found in the database.")
+
     def __str__(self):
         s = ''
         for account in self.account_database:
@@ -63,5 +71,5 @@ my_account_DB.search_public("0003").deposit(50)
 print(my_account_DB)
 my_account_DB.search_public("0003").withdraw(100)
 print(my_account_DB)
-my_account_DB.search_public("0010").deposit(50)
+my_account_DB.delete("0001")
 print(my_account_DB)
